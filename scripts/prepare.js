2bcb3a8fc396179211b4e9e3ba55bbb94ac7fda9
carla <carlasofiamendes@outlook.com>
2023-05-10 19:24:46 +0100

    Update prepare.js

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -3,7 +3,7 @@
 // See Disable husky in CI/Docker/Prod
 // https://typicode.github.io/husky/#/?id=disable-husky-in-cidockerprod
 // to avoid permissions issue with npm ci in a Docker container
-//
+// hello
 const isCi = process.env.CI !== undefined
 if (!isCi) {
   require('husky').install()
